1b8018bd5cfe06e514dd908fa5c72f4a3bb39f9f
Samuel Murillo <smurillo@asap-consulting.com.ar>
2019-03-15 17:25:16 -0300

    Add retrieve artist. Return 422 when artist exists

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,9 @@
 #include <bsoncxx/types.hpp>
 #include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
+#include <mongocxx/exception/server_error_code.hpp>
+#include <mongocxx/exception/bulk_write_exception.hpp>
+#include <mongocxx/exception/operation_exception.hpp>
 
 using namespace std;
 using namespace Pistache;
@@ -58,19 +61,19 @@ private:
         using namespace Rest;
 
         Routes::Get(router, "/ready", Routes::bind(&MetalAPI::handleReady, this));
-        Routes::Get(router, "/artist", Routes::bind(&MetalAPI::retrieve_artists, this));
+        Routes::Get(router, "/artist", Routes::bind(&MetalAPI::list_artists, this));
         Routes::Post(router, "/artist", Routes::bind(&MetalAPI::create_artist, this));
+        Routes::Get(router, "/artist/:name", Routes::bind(&MetalAPI::retrieve_artist, this));
     }
 
     void handleReady(const Rest::Request &, Http::ResponseWriter response) {
         response.send(Http::Code::Ok, "1");
     }
 
-    void retrieve_artists(const Rest::Request &request, Http::ResponseWriter response) {
+    void list_artists(const Rest::Request &request, Http::ResponseWriter response) {
         Document json_response;
         json_response.SetObject();
         Value results(kArrayType);
-        cout << "Querying metal_api.artists" << endl;
         Document::AllocatorType &allocator = json_response.GetAllocator();
         auto collection = mongo_conn["metal_api"]["artists"];
         auto order = document{} << "name" << 1 << finalize;
@@ -104,10 +107,48 @@ private:
         response.send(Http::Code::Ok, buffer.GetString());
     }
 
+    void retrieve_artist(const Rest::Request &request, Http::ResponseWriter response) {
+        auto name = request.param(":name").as<std::string>();
+        Document json_response;
+        json_response.SetObject();
+        Value results(kArrayType);
+        Document::AllocatorType &allocator = json_response.GetAllocator();
+        auto collection = mongo_conn["metal_api"]["artists"];
+
+        bsoncxx::stdx::optional<bsoncxx::document::value> result =
+                collection.find_one(document{} << "name" << name << finalize);
+        Http::Code code;
+        string body;
+        if (result) {
+            bsoncxx::document::view view = result.value().view();
+
+            code = Http::Code::Ok;
+            auto _id_element = view["_id"];
+            auto name_element = view["name"];
+
+            const string _id = _id_element.get_oid().value.to_string();
+            const string band_name = name_element.get_utf8().value.to_string();
+            json_response.AddMember("_id", _id, allocator);
+            json_response.AddMember("name", band_name, allocator);
+
+            StringBuffer buffer;
+            Writer<StringBuffer> writer(buffer);
+            json_response.Accept(writer);
+            body = buffer.GetString();
+        } else {
+            code = Http::Code::Not_Found;
+        };
+
+        response.headers().add<Http::Header::ContentType>(MIME(Application, Json));
+        response.send(code, body);
+    }
+
     void create_artist(const Rest::Request &request, Http::ResponseWriter response) {
         Document payload, json_response;
         json_response.SetObject();
         payload.Parse(request.body());
+        Http::Code code;
+        string body;
 
         Document::AllocatorType &allocator = json_response.GetAllocator();
 
@@ -119,20 +160,34 @@ private:
                 << "name" << name.GetString()
                 << bsoncxx::builder::stream::finalize;
         bsoncxx::document::view view = doc_value.view();
-        bsoncxx::stdx::optional<mongocxx::result::insert_one> result = artists.insert_one(view);
-        const bsoncxx::types::value& result_id = result->inserted_id();
-        const auto _id = result_id.get_oid().value.to_string();
+        try {
+            bsoncxx::stdx::optional<mongocxx::result::insert_one> result = artists.insert_one(view);
+            const bsoncxx::types::value &result_id = result->inserted_id();
+            const auto _id = result_id.get_oid().value.to_string();
 
-        json_response.AddMember("_id", _id, allocator);
-        json_response.AddMember("name", (string) name.GetString(), allocator);
+            json_response.AddMember("_id", _id, allocator);
+            json_response.AddMember("name", (string) name.GetString(), allocator);
 
-        StringBuffer buffer;
-        Writer<StringBuffer> writer(buffer);
-        json_response.Accept(writer);
+            StringBuffer buffer;
+            Writer<StringBuffer> writer(buffer);
+            json_response.Accept(writer);
 
-        cout << buffer.GetString() << endl;
+            cout << buffer.GetString() << endl;
+            body = buffer.GetString();
+            code = Http::Code::Created;
+        } catch (const mongocxx::bulk_write_exception& e) {
+            if (e.code().value() == 11000) {
+                code = Http::Code::Unprocessable_Entity;
+                body = "";  // TODO this
+            } else {
+                // TODO GOTO next catch
+            }
+        } catch (const std::system_error& e) {
+            code = Http::Code::Internal_Server_Error;
+            body = e.what();
+        }
         response.headers().add<Http::Header::ContentType>(MIME(Application, Json));
-        response.send(Http::Code::Ok, buffer.GetString());
+        response.send(code, body);
     }
 
 };
